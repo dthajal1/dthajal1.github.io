a678955681145f93fa69a3c69b29da19b53c2eb4
Diraj Thajali <thajalidiraj@gmail.com>
2024-08-09 10:01:04 -0700

    minor

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -17,13 +17,20 @@ const AboutMe = () => {
           <Divider />
           <br />
           <Typography paragraph>
-            I am a software engineer with a focus in backend and full stack development. I graduated from UC Berkeley with a degree in EECS and I have expertise in Python, Next.js, Node/Express.js and React.js. I have built personal projects with them and also have used them extensively in my career thus far. You can learn more about them below.
+          I am a software engineer with a focus on backend and full stack development. 
+          I graduated from UC Berkeley with a degree in EECS and have hands-on experience 
+          with Python, Next.js, Node/Express.js, and React.js. I have built personal projects 
+          with them and also have used them extensively in my career thus far.
           </Typography>
           <Typography paragraph>
-          Before beginning my career in software engineering, I spent about a year playing professional soccer. The drive and determination I applied to excel in soccer now fuels my passion for technology. I believe that with the right mindset, we can achieve anything—whether it’s mastering a sport or developing cutting-edge software.
+          Before beginning my career in software engineering, I went on to play professional soccer abroad. 
+          Through my soccer journey, I have learned that with right mindset, attitude, heart and grit,
+          we can achieve anything--whether it’s mastering a sport, learning to play guitar or developing cutting-edge software. 
+          Now, the same drive and determination I used to excel in soccer, fuels my passion for technology.
           </Typography>
           <Typography paragraph>
-          In my free time, I enjoy coaching kids in soccer, exploring new tech trends, and working on personal projects. Always eager to learn more and take on new challenges!
+          In my free time, I enjoy coaching kids in soccer, exploring new tech trends, and building cool projects.
+          Always eager to learn more and take on new challenges.
           </Typography>
           <Typography paragraph>
           Feel free to connect with me if you’d like to chat about tech, soccer, or anything in between!
